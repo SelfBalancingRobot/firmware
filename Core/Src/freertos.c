f960c7e30b915307768801123a28fcb3abafbfe8
Ozkerror <ozkerror@gmail.com>
2026-06-11 13:17:30 +0200

    aktualizacja parsowanie komend, zmiana warunku wywolyawnia taska regulacji, dodanie taska do wysylania logów, dodanie trybow pracy robota

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -30,7 +30,7 @@
 #include "MPU6050.h"
 #include "LED_matrix.h"
 #include "balance_regulator.h"
-#include <ctype.h>
+#include "command_parser.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -59,9 +59,11 @@ motor_t motor1 = {
   .encoder_prev_count = 0,
   .encoder_resolution = 700,
   .measured_rpm = 0,
+  .ramped_rpm = 0,
+  .ramp_step = 8,
   .max_rpm = 251.0f,
-  .regulator_Kp = 1.5f,
-  .regulator_Ki = 0.05f,
+  .regulator_Kp = 0.8f,
+  .regulator_Ki = 0.0f,
   .integral = 0,
   .sign = -1
 
@@ -75,9 +77,11 @@ motor_t motor2 = {
   .encoder_prev_count = 0,
   .encoder_resolution = 700,
   .measured_rpm = 0,
+  .ramped_rpm = 0,
+  .ramp_step = 8,
   .max_rpm = 251.0f,
-  .regulator_Kp = 1.5f,
-  .regulator_Ki = 0.2f,
+  .regulator_Kp = 0.8f,
+  .regulator_Ki = 0.0f,
   .integral = 0,
   .sign = 1
 };
@@ -96,11 +100,37 @@ uart_t bluetooth = {
 };
 balance_pid_t balance_pid = {
 	.target_angle = 0.0f,
-	.Kp = 20.0f,
+	.Kp = 10.0f,
 	.Ki = 0.0f,
-	.Kd = 0.0f,
+	.Kd = 5.0f,
 	.integral = 0.0f,
-	.prev_error = 0.0f
+	.prev_error = 0.0f,
+	.rpm_death_band = 10.0f
+
+};
+
+mpu_kalman_t mpu_kalman = {
+	.Q_angle = 0.001f,
+	.Q_bias = 0.003f,
+	.R_measure = 0.03f,
+	.angle = 0.0f,
+	.bias = 0.0f,
+	.P = {{0.0f, 0.0f},{0.0f, 0.0f}},
+	.initialized = false
+};
+
+robot_mode_t robot_mode = BALANCE;
+float callibration_rpm = 0;
+bool send_imu_offsets_flag = false;
+
+command_context_t command_context = {
+	.balance_pid = &balance_pid,
+	.motor1 = &motor1,
+	.motor2 = &motor2,
+	.mode = &robot_mode,
+	.rpm = &callibration_rpm,
+	.kalman = &mpu_kalman,
+	.send_imu_offsets_flag = &send_imu_offsets_flag
 };
 
 float current_angle = 0.0f;
@@ -113,12 +143,12 @@ const osThreadAttr_t ControlTask_attributes = {
   .stack_size = 128 * 4,
   .priority = (osPriority_t) osPriorityRealtime,
 };
-/* Definitions for BluetoothTask */
-osThreadId_t BluetoothTaskHandle;
-const osThreadAttr_t BluetoothTask_attributes = {
-  .name = "BluetoothTask",
+/* Definitions for SendLogsTask */
+osThreadId_t SendLogsTaskHandle;
+const osThreadAttr_t SendLogsTask_attributes = {
+  .name = "SendLogsTask",
   .stack_size = 1024 * 4,
-  .priority = (osPriority_t) osPriorityBelowNormal,
+  .priority = (osPriority_t) osPriorityLow2,
 };
 /* Definitions for LedMatrixTask */
 osThreadId_t LedMatrixTaskHandle;
@@ -126,6 +156,18 @@ const osThreadAttr_t LedMatrixTask_attributes = {
   .name = "LedMatrixTask",
   .stack_size = 128 * 4,
   .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for ReceiveCmdTask */
+osThreadId_t ReceiveCmdTaskHandle;
+const osThreadAttr_t ReceiveCmdTask_attributes = {
+  .name = "ReceiveCmdTask",
+  .stack_size = 256 * 4,
+  .priority = (osPriority_t) osPriorityLow4,
+};
+/* Definitions for HardwareInitSemaphore */
+osSemaphoreId_t HardwareInitSemaphoreHandle;
+const osSemaphoreAttr_t HardwareInitSemaphore_attributes = {
+  .name = "HardwareInitSemaphore"
 };
 /* Definitions for UartInitSemaphore */
 osSemaphoreId_t UartInitSemaphoreHandle;
@@ -139,8 +181,9 @@ const osSemaphoreAttr_t UartInitSemaphore_attributes = {
 /* USER CODE END FunctionPrototypes */
 
 void StartControlTask(void *argument);
-void StartBluetoothTask(void *argument);
+void StartSendLogsTask(void *argument);
 void StartLedMatrixTask(void *argument);
+void StartReceiveCmdTask(void *argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
@@ -159,6 +202,9 @@ void MX_FREERTOS_Init(void) {
   /* USER CODE END RTOS_MUTEX */
 
   /* Create the semaphores(s) */
+  /* creation of HardwareInitSemaphore */
+  HardwareInitSemaphoreHandle = osSemaphoreNew(1, 1, &HardwareInitSemaphore_attributes);
+
   /* creation of UartInitSemaphore */
   UartInitSemaphoreHandle = osSemaphoreNew(1, 1, &UartInitSemaphore_attributes);
 
@@ -178,11 +224,14 @@ void MX_FREERTOS_Init(void) {
   /* creation of ControlTask */
   ControlTaskHandle = osThreadNew(StartControlTask, NULL, &ControlTask_attributes);
 
-  /* creation of BluetoothTask */
-  BluetoothTaskHandle = osThreadNew(StartBluetoothTask, NULL, &BluetoothTask_attributes);
+  /* creation of SendLogsTask */
+  SendLogsTaskHandle = osThreadNew(StartSendLogsTask, NULL, &SendLogsTask_attributes);
 
   /* creation of LedMatrixTask */
   LedMatrixTaskHandle = osThreadNew(StartLedMatrixTask, NULL, &LedMatrixTask_attributes);
+
+  /* creation of ReceiveCmdTask */
+  ReceiveCmdTaskHandle = osThreadNew(StartReceiveCmdTask, NULL, &ReceiveCmdTask_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -204,8 +253,9 @@ void MX_FREERTOS_Init(void) {
 void StartControlTask(void *argument)
 {
   /* USER CODE BEGIN StartControlTask */
-  osSemaphoreAcquire(UartInitSemaphoreHandle, 0);
-  uint32_t Ts_ms = 10;
+  osSemaphoreAcquire(HardwareInitSemaphoreHandle, 0);
+  uint32_t Ts_ms = 4;
+  uint32_t nextWake = osKernelGetTickCount();
   float Ts_s = (float)Ts_ms / 1000.0f;
   float ax = 0.0f;
   float ay = 0.0f;
@@ -221,111 +271,73 @@ void StartControlTask(void *argument)
 
   mpu_count_accel_offset();
   mpu_count_gyro_offset();
-  osSemaphoreRelease(UartInitSemaphoreHandle);
+  osSemaphoreRelease(HardwareInitSemaphoreHandle);
 
   /* Infinite loop */
   for(;;)
   {
+	nextWake += Ts_ms;
 	mpu_get_accel(&ax, &ay, &az);
 	mpu_get_gyro(&gx, &gy, &gz);
-	current_angle = mpu_count_angle(ay, az, gx, Ts_s);
-    if(current_angle > 45.0f || current_angle < -45.0f) {
-        motor_set_rpm(&motor1, 0, Ts_s);
-        motor_set_rpm(&motor2, 0, Ts_s);
-        balance_pid.integral = 0; // zeruj całkę
-        motor1.integral = 0;
-        motor2.integral = 0;
-    }else{
-    	float target_rpm = balance_count_target_rpm(&balance_pid, current_angle, Ts_s);
-    	motor_set_rpm(&motor2, target_rpm, Ts_s);
-    	motor_set_rpm(&motor1, target_rpm, Ts_s);
-    }
+	current_angle = mpu_count_angle_Kalman(&mpu_kalman, ay, az, gx, Ts_s);
+	if(robot_mode == BALANCE){
+	    if(current_angle > 45.0f || current_angle < -45.0f) {
+	        motor_set_rpm(&motor1, 0, Ts_s);
+	        motor_set_rpm(&motor2, 0, Ts_s);
+	        balance_pid.integral = 0; // zeruj całkę
+	        motor1.integral = 0;
+	        motor2.integral = 0;
+	    }else{
+	    	float target_rpm = balance_count_target_rpm(&balance_pid, current_angle, Ts_s);
+	    	motor_set_rpm(&motor2, target_rpm, Ts_s);
+	    	motor_set_rpm(&motor1, target_rpm, Ts_s);
+	    }
+	}else if(robot_mode == MOTOR_CALIBRATION){
+		motor_set_rpm(&motor1, callibration_rpm, Ts_s);
+		motor_set_rpm(&motor2, callibration_rpm, Ts_s);
+	}
 
-    osDelay(Ts_ms);
+    osDelayUntil(nextWake);
   }
   /* USER CODE END StartControlTask */
 }
 
-/* USER CODE BEGIN Header_StartBluetoothTask */
+/* USER CODE BEGIN Header_StartSendLogsTask */
 /**
-* @brief Function implementing the BluetoothTask thread.
+* @brief Function implementing the SendLogsTask thread.
 * @param argument: Not used
 * @retval None
 */
-/* USER CODE END Header_StartBluetoothTask */
-void StartBluetoothTask(void *argument)
+/* USER CODE END Header_StartSendLogsTask */
+void StartSendLogsTask(void *argument)
 {
-  /* USER CODE BEGIN StartBluetoothTask */
-  char received_message[30] = {""};
-  uint16_t p = 0;
-  uint16_t i = 0;
-  uint16_t d = 0;
-  my_uart_init(&bluetooth);
-  osSemaphoreAcquire(UartInitSemaphoreHandle, osWaitForever);
+  /* USER CODE BEGIN StartSendLogsTask */
   /* Infinite loop */
+  osSemaphoreAcquire(HardwareInitSemaphoreHandle, osWaitForever);
+  osSemaphoreAcquire(UartInitSemaphoreHandle, osWaitForever);
   for(;;)
   {
-
-	  my_uart_printf(&bluetooth, "motor1 rpm = %.2f\n", motor1.measured_rpm);
-	  my_uart_printf(&bluetooth, "motor2 rpm = %.2f\n", motor2.measured_rpm);
-	  my_uart_printf(&bluetooth, "current angle = %.2f\n", current_angle);
-
-	  if(my_uart_get_message(&bluetooth, received_message) == true){
-		  for(uint8_t idx = 0; idx < sizeof(received_message); idx++){
-			  if(received_message[idx] == 'p'){
-				  if(isdigit(received_message[idx+1]) && isdigit(received_message[idx+2]) && isdigit(received_message[idx+3])){
-					  p = (received_message[idx + 1] - '0') * 100 +
-						  (received_message[idx + 2] - '0') * 10 +
-						  received_message[idx + 3] - '0';
-					  balance_pid.Kp = p / 10 + (float)(p % 10)/ 10;
-				  }
-
-			  }
-			  if(received_message[idx] == 'i'){
-				  if(isdigit(received_message[idx+1]) && isdigit(received_message[idx+2]) && isdigit(received_message[idx+3])){
-					  i = (received_message[idx + 1] - '0') * 100 +
-						  (received_message[idx + 2] - '0') * 10 +
-						  received_message[idx + 3] - '0';
-					  balance_pid.Ki = i / 100 + (float)(i % 100)/ 100;
-				  }
-
-			  }
-			  if(received_message[idx] == 'd'){
-				  if(isdigit(received_message[idx+1]) && isdigit(received_message[idx+2]) && isdigit(received_message[idx+3])){
-					  d = (received_message[idx + 1] - '0') * 100 +
-						  (received_message[idx + 2] - '0') * 10 +
-						  received_message[idx + 3] - '0';
-					  balance_pid.Kd = d / 100 + (float)(d % 100)/ 100;
-				  }
-
-			  }
-			  if(received_message[idx] == 'P'){
-				  if(isdigit(received_message[idx+1]) && isdigit(received_message[idx+2]) && isdigit(received_message[idx+3])){
-					  p = (received_message[idx + 1] - '0') * 100 +
-						  (received_message[idx + 2] - '0') * 10 +
-						  received_message[idx + 3] - '0';
-					  float P = p / 100 + (float)(p % 100)/ 100;
-					  motor1.regulator_Kp = P;
-					  motor2.regulator_Kp = P;
-				  }
-
-			  }
-			  if(received_message[idx] == 'I'){
-				  if(isdigit(received_message[idx+1]) && isdigit(received_message[idx+2]) && isdigit(received_message[idx+3])){
-					  i = (received_message[idx + 1] - '0') * 100 +
-						  (received_message[idx + 2] - '0') * 10 +
-						  received_message[idx + 3] - '0';
-					  float I = i / 100 + (float)(i % 100)/ 100;
-					  motor1.regulator_Ki = I;
-					  motor2.regulator_Ki = I;
-				  }
-
-			  }
-		  }
-	  }
-	  osDelay(500);
+	my_uart_printf(&bluetooth, "Motor1 rpm = %.2f\n", motor1.measured_rpm);
+	my_uart_printf(&bluetooth, "Motor2 rpm = %.2f\n", motor2.measured_rpm);
+	my_uart_printf(&bluetooth, "Current angle = %.2f\n", current_angle);
+	my_uart_printf(&bluetooth, "Motor1:\tKp = %.2f\tKi = %.2f\n", motor1.regulator_Kp, motor1.regulator_Ki);
+	my_uart_printf(&bluetooth, "Motor2:\tKp = %.2f\tKi = %.2f\n", motor2.regulator_Kp, motor2.regulator_Ki);
+	my_uart_printf(&bluetooth, "Motor1: step = %.2f\n", motor1.ramp_step);
+	my_uart_printf(&bluetooth, "Motor2: step = %.2f\n", motor2.ramp_step);
+	my_uart_printf(&bluetooth, "Balancer:\tKp = %.2f\tKi=%.2f\tKd=%.2f\n", balance_pid.Kp, balance_pid.Ki, balance_pid.Kd);
+    my_uart_printf(&bluetooth, "RPM death band = %.2f\n", balance_pid.rpm_death_band);
+    my_uart_printf(&bluetooth, "Kalman:\tR_m = %.4f\tQ_a = %.4f\tQ_b = %.4f\n", mpu_kalman.R_measure, mpu_kalman.Q_angle, mpu_kalman.Q_bias);
+	if(send_imu_offsets_flag == true){
+    	float ax, ay, az, gx, gy, gz;
+    	mpu_get_accel_offset(&ax, &ay, &az);
+    	mpu_get_gyro_offset(&gx, &gy, &gz);
+    	my_uart_printf(&bluetooth, "Accelerometer offsets: x=%.2f\ty=%.2f\tz=%.2f\n", ax, ay, az);
+    	my_uart_printf(&bluetooth, "Gyroscope offsets: x=%.2f\ty=%.2f\tz=%.2f\n", gx, gy, gz);
+    	send_imu_offsets_flag = false;
+    }
+	osDelay(500);
   }
-  /* USER CODE END StartBluetoothTask */
+  /* USER CODE END StartSendLogsTask */
 }
 
 /* USER CODE BEGIN Header_StartLedMatrixTask */
@@ -380,12 +392,40 @@ void StartLedMatrixTask(void *argument)
 	  LED_matrix_load_buffer(&ledMatrix2, pattern6);
 	  sixSeven = true;
 	}
-
+	LED_matrix_rotate_buffer(&ledMatrix1, ROTATE_CW);
+	LED_matrix_rotate_buffer(&ledMatrix2, ROTATE_CW);
 	LED_matrix_draw_buffer(&ledMatrix1);
 	LED_matrix_draw_buffer(&ledMatrix2);
 	osDelay(1000);
   }
   /* USER CODE END StartLedMatrixTask */
+}
+
+/* USER CODE BEGIN Header_StartReceiveCmdTask */
+/**
+* @brief Function implementing the ReceiveCmdTask thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartReceiveCmdTask */
+void StartReceiveCmdTask(void *argument)
+{
+  /* USER CODE BEGIN StartReceiveCmdTask */
+  /* Infinite loop */
+  char received_message[30] = {""};
+  osSemaphoreAcquire(HardwareInitSemaphoreHandle, osWaitForever);
+  osSemaphoreRelease(HardwareInitSemaphoreHandle);
+  osSemaphoreAcquire(UartInitSemaphoreHandle, 0);
+  my_uart_init(&bluetooth);
+  osSemaphoreRelease(UartInitSemaphoreHandle);
+  for(;;)
+  {
+	if(my_uart_get_message(&bluetooth, received_message) == true){
+	  parse_command(&command_context, received_message);
+	}
+    osDelay(50);
+  }
+  /* USER CODE END StartReceiveCmdTask */
 }
 
 /* Private application code --------------------------------------------------*/
